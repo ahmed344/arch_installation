35e058b38f3c68201476c8cc2faf8979f33f6f2f
ahmed344 <ahmedsharaf483@gmail.com>
2021-06-10 04:52:02 +0200

    remove linux headers

--- basic_packages.sh
+++ basic_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers alsa-utils pulseaudio bash-completion
+pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
