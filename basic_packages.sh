dcc3d2de6dddef7e2b0ec44456fd67897f8b7a54
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-06-13 01:54:01 +0200

    add ssh and enable it

--- basic_packages.sh
+++ basic_packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion
+pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion openssh
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
+systemctl enable sshd
