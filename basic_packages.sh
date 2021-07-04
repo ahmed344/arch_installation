4578d55419cd144b9ad25d06dff09e54defb7d4f
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-07-04 03:30:51 +0200

    add the user and groups and some utilities

--- basic_packages.sh
+++ basic_packages.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion openssh
+pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion openssh xdg-user-dirs xdg-utils
 
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable sshd
+
+useradd -m ahmed
+usermod -aG wheel,audio,video,optical,storage ahmed
