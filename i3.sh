fbb83be2dd0038678603ed53f4ae819d18152921
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-07-04 06:13:09 +0200

    create first draft

---
+++ i3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo pacman -S --noconfirm xorg xorg-xrandr arandr xorg-xdm xorg-xinit mesa i3 dmenu i3status i3lock ttf-dejavu polkit lxsession xbindkeys udiskie udisks2 ntfs-3g wget htop pcmanfm gimp vlc firefox thunderbird
+
+echo “exec i3” > ~/.xsession
+chmod +x ~/.xsession
+
+sudo systemctl enable xdm.service
+
+# sudo pacman -S tlp tlp-rdw
+# sudo systemctl enable tlp.service
+# sudo systemctl start tlp.service
