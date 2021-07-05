9fe1af9c4e3a8e73cc98effc0cd6d5336867fb6f
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-07-05 23:29:37 +0200

    add terminator

--- i3.sh
+++ i3.sh
@@ -3,7 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo pacman -S --noconfirm xorg xorg-xrandr arandr xorg-xdm xorg-xinit mesa i3 i3status i3blocks i3lock dmenu ttf-dejavu polkit lxsession xbindkeys udiskie udisks2 ntfs-3g wget htop pcmanfm gimp vlc firefox thunderbird
+sudo pacman -S --noconfirm xorg xorg-xrandr arandr xorg-xdm xorg-xinit mesa i3 i3status i3blocks i3lock dmenu terminator ttf-dejavu polkit lxsession xbindkeys udiskie udisks2 ntfs-3g wget htop pcmanfm gimp vlc firefox thunderbird
 
 echo “exec i3” > ~/.xsession
 chmod +x ~/.xsession
