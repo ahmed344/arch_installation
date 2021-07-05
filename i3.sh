a24eefa3400fe9825c7dbfa1a98e7280c23c044d
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-07-05 23:38:55 +0200

    comment out xdm

--- i3.sh
+++ i3.sh
@@ -3,12 +3,13 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo pacman -S --noconfirm xorg xorg-xrandr arandr xorg-xdm xorg-xinit mesa i3 i3status i3blocks i3lock dmenu terminator ttf-dejavu polkit lxsession xbindkeys udiskie udisks2 ntfs-3g wget htop pcmanfm gimp vlc firefox thunderbird
+sudo pacman -S --noconfirm xorg xorg-xrandr arandr xorg-xinit mesa i3 i3status i3blocks i3lock dmenu terminator ttf-dejavu polkit lxsession xbindkeys udiskie udisks2 ntfs-3g wget htop pcmanfm gimp vlc firefox thunderbird
 
-echo “exec i3” > ~/.xsession
-chmod +x ~/.xsession
 
-sudo systemctl enable xdm.service
+# sudo pacman -S xorg-xdm
+# sudo systemctl enable xdm.service
+# echo “exec i3” > ~/.xsession
+# chmod +x ~/.xsession
 
 # sudo pacman -S tlp tlp-rdw
 # sudo systemctl enable tlp.service
