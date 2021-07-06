dad8a2add857fbbb8f4cc32f58f79777a0f12051
Ahmed ABDELRAHMAN <51710420+ahmed344@users.noreply.github.com>
2021-07-06 23:05:39 +0200

    update to EFI file system

--- basic_packages.sh
+++ basic_packages.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pacman -S --noconfirm grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion openssh xdg-user-dirs xdg-utils
+pacman -S --noconfirm efibootmgr os-prober grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel alsa-utils pulseaudio bash-completion openssh xdg-user-dirs xdg-utils
 
-grub-install /dev/sda
+# grub-install /dev/sda
+# grub-mkconfig -o /boot/grub/grub.cfg
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
 
 systemctl enable NetworkManager
 systemctl enable sshd
